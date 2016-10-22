0699eedb3995b3eee164fb40eeadbe6185c8bada
LuiVC <luisavc09@gmail.com>
2016-10-22 14:45:57 -0500

    Se agregan las promeras Actividades

---
+++ AplicativoFinal/app/src/main/java/com/uniquindio/edu/aplicativofinal/AgregarParticipante.java
@@ -0,0 +1,13 @@
+package com.uniquindio.edu.aplicativofinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgregarParticipante extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_participante);
+    }
+}
